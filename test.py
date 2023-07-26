9ea2e0622804363482c35685ba50e3aa4d588854
Bharath <bharathkote7@gmail.com>
2023-07-26 18:21:26 +0530

    Calculate the sum of the digits

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
 a= int(input("Enter the number:"))
 ns=str(a)
+
+
+# To find the sum of the digits -  branch sumofdig
+total=0
+for i in ns:
+    total+=int(i)
+
+print("The sum of the digits:",total)
